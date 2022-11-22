7be401113469e35edb9585d34d06aff41f4f7891
Yobel <vega.yobel.2@gmail.com>
2022-11-23 04:38:25 +0700

    Landing page

---
+++ src/assets/images/index.js
@@ -0,0 +1,8 @@
+import Miya from "./miya.png";
+import Lunox from "./lunox.png";
+import Zilong from "./zilong.png";
+import Hanabi from "./hanabi.png";
+import Image1 from "./image1.jpg";
+import Balmond from "./balmond.png";
+
+export { Lunox, Zilong, Hanabi, Miya, Image1, Balmond };
